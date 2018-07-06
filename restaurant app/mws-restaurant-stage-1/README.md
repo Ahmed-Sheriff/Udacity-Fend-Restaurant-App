47cbd06ee32675ccfa94fe67a42778401fc4878a
Ahmed-Sheriff <38168065+Ahmed-Sheriff@users.noreply.github.com>
2018-07-06 11:50:43 +0200

    Add files via upload

---
+++ restaurant app/mws-restaurant-stage-1/README.md
@@ -0,0 +1,30 @@
+
+# Restaurant Reviews App
+
+## Project introduction
+
+- The mainly purpose of the app is to navigate to your prefered restaurants to know thier locations, opening and closing times and address besides the customers rivewers about them . 
+You can easly choose your restaurant by doing a filtering option either with locations or by cuisines.
+
+## Project installation 
+
+1- Download the repo 
+2- Run the server
+
+## Project features 
+
+### Responsiveness : 
+
+- The app is responsive to all of differents device screens 
+
+### Accessibility  : 
+
+- The app has an accessibility feature for users who have motor impairments to navigate easly through the page 
+
+### Offline mode : 
+
+- If the network disconnected , the app will show and work on offline mode and it will show the all assets files that app is needed   
+
+### Project dependencies :
+
+Mapbox Api for map   
---
+++ restaurant app/mws-restaurant-stage-1/sw.js
@@ -0,0 +1,102 @@
+
+//Install the service worker
+
+var CACHE_NAME = 'restaurant';
+var urlsToCache = [
+  	'/',
+  'restaurant.html',
+  'index.html',
+  'css/styles.css',
+  'css/responsive.css',	
+  'js/main.js',
+  'js/restaurant_info.js',
+  'js/dbhelper.js',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'	
+];
+
+self.addEventListener('install', function(event){
+
+	 event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+
+});
+
+//Caches and responses 
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+
+			// Resolve the custom 404 page
+		// return caches.open(CACHE_NAME).then(function(cache) {
+			    
+		// 	if (event.request.url.indexOf('test') < 0) {
+		// 	     cache.put(event.request.url, response.clone());
+		// 	   }
+			    
+		// 	    return response;
+
+		// 	  });
+
+			
+      })
+  );
+});	
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to retriev also from cache   
+      
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to also get retrieved from cache 
+            
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
